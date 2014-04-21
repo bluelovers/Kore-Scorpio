83cad1c57fb17eb0b06b0887c9bff894ea3c905e
bluelovers <codelovers@users.sourceforge.net>
2014-04-22 07:32:35 +0800

    5.11.2.2 (Step: 3)
    Step: 3

--- Scorpio-Source/Scorpio_version.pl
+++ Scorpio-Source/Scorpio_version.pl
@@ -1,8 +1,8 @@
 # Scorpio version config //
 
-$sc_v{'Scorpio'}{'version'}	= "5.11.2.1";
-$sc_v{'Scorpio'}{'update'}	= "02/11/05 - 08:16:44";
-$sc_v{'Scorpio'}{'regkey'}	= "6c4f760ec0364f239810187ac068d5a0";
+$sc_v{'Scorpio'}{'version'}	= "5.11.2.2";
+$sc_v{'Scorpio'}{'update'}	= "02/11/05 - 08:17:41";
+$sc_v{'Scorpio'}{'regkey'}	= "693313ca04ee946d48c515631da1b941";
 
 $sc_v{'Scorpio'}{'checkVer'}	= 0;
 
