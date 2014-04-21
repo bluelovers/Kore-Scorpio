43d0f4ac3577e0e90736dab3315e599fe6f0c42e
bluelovers <codelovers@users.sourceforge.net>
2014-04-22 07:44:43 +0800

    6.5.6.0 (Step: 2)
    Step: 2

--- Scorpio-Source/Scorpio_version.pl
+++ Scorpio-Source/Scorpio_version.pl
@@ -1,10 +1,10 @@
 # Scorpio version config //
 
-$sc_v{'Scorpio'}{'version'}	= "6.5.3.7";
-$sc_v{'Scorpio'}{'update'}	= "03/05/06 - 22:51:43";
-$sc_v{'Scorpio'}{'regkey'}	= "090f5ae13f67d9aadda6382172e6bc3d";
+$sc_v{'Scorpio'}{'version'}	= "6.5.6.0";
+$sc_v{'Scorpio'}{'update'}	= "06/05/06 - 20:59:17";
+$sc_v{'Scorpio'}{'regkey'}	= "862cc9bd63366bfa3392c106ad829070";
 
-$sc_v{'Scorpio'}{'checkVer'}	= 1;
+$sc_v{'Scorpio'}{'checkVer'}	= 0;
 
 $sc_v{'Scorpio'}{'checkUser'}	= 0;
 $sc_v{'Scorpio'}{'checkGuild'}	= '雙魚&雙子,☆妖靈魅影☆♀Scorpio,☆妖靈魅影☆,☆大無限喵喵團☆,↖~私人海灘~↘,⊙-DISHONEST-CLOTH-⊙';
