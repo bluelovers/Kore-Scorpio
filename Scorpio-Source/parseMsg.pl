55564789e155d20149b212ed236119aa2ef4d215
bluelovers <codelovers@users.sourceforge.net>
2014-04-22 07:38:05 +0800

    6.5.3.1 (Step: 4)
    Step: 4

--- Scorpio-Source/parseMsg.pl
+++ Scorpio-Source/parseMsg.pl
@@ -327,47 +327,64 @@ $num, $servers[$num]{'name'}, $servers[$num]{'users'}, $servers[$num]{'ip'}, $se
 		}
 		for($i = $startVal; $i < $msg_size; $i+=106) {
 #exp display bugfix - chobit andy 20030129
+#			$num = unpack("C1", substr($msg, $i + 104, 1));
+#			$chars[$num]{'exp'} = unpack("L1", substr($msg, $i + 4, 4));
+#			$chars[$num]{'zenny'} = unpack("L1", substr($msg, $i + 8, 4));
+#			$chars[$num]{'exp_job'} = unpack("L1", substr($msg, $i + 12, 4));
+#			$chars[$num]{'lv_job'} = unpack("C1", substr($msg, $i + 16, 1));
+#			$chars[$num]{'hp'} = unpack("S1", substr($msg, $i + 42, 2));
+#			$chars[$num]{'hp_max'} = unpack("S1", substr($msg, $i + 44, 2));
+#			$chars[$num]{'hp_string'} = sprintf("%5d", $chars[$num]{'hp'})."/".sprintf("%-5d", $chars[$num]{'hp_max'}) if ($chars[$num]{'hp_max'} ne "");
+#			$chars[$num]{'sp'} = unpack("S1", substr($msg, $i + 46, 2));
+#			$chars[$num]{'sp_max'} = unpack("S1", substr($msg, $i + 48, 2));
+#			$chars[$num]{'sp_string'} = sprintf("%5d", $chars[$num]{'sp'})."/".sprintf("%-5d", $chars[$num]{'sp_max'}) if ($chars[$num]{'sp_max'} ne "");
+#			$chars[$num]{'jobID'} = unpack("C1", substr($msg, $i + 52, 1));
+#			$chars[$num]{'lv'} = unpack("C1", substr($msg, $i + 58, 1));
+#			($chars[$num]{'name'}) = substr($msg, $i + 74, 24) =~ /([\s\S]*?)\000/;
+#			$chars[$num]{'str'} = unpack("C1", substr($msg, $i + 98, 1));
+#			$chars[$num]{'agi'} = unpack("C1", substr($msg, $i + 99, 1));
+#			$chars[$num]{'vit'} = unpack("C1", substr($msg, $i + 100, 1));
+#			$chars[$num]{'int'} = unpack("C1", substr($msg, $i + 101, 1));
+#			$chars[$num]{'dex'} = unpack("C1", substr($msg, $i + 102, 1));
+#			$chars[$num]{'luk'} = unpack("C1", substr($msg, $i + 103, 1));
+#			$chars[$num]{'sex'} = $sc_v{'input'}{'accountSex2'};
+			
 			$num = unpack("C1", substr($msg, $i + 104, 1));
 			$chars[$num]{'exp'} = unpack("L1", substr($msg, $i + 4, 4));
 			$chars[$num]{'zenny'} = unpack("L1", substr($msg, $i + 8, 4));
 			$chars[$num]{'exp_job'} = unpack("L1", substr($msg, $i + 12, 4));
 			$chars[$num]{'lv_job'} = unpack("C1", substr($msg, $i + 16, 1));
-			$chars[$num]{'hp'} = unpack("S1", substr($msg, $i + 42, 2));
+			#$chars[$num]{'hp'} = unpack("S1", substr($msg, $i + 42, 2));
 			$chars[$num]{'hp_max'} = unpack("S1", substr($msg, $i + 44, 2));
-			$chars[$num]{'hp_string'} = sprintf("%5d", $chars[$num]{'hp'})."/".sprintf("%-5d", $chars[$num]{'hp_max'}) if ($chars[$num]{'hp_max'} ne "");
-			$chars[$num]{'sp'} = unpack("S1", substr($msg, $i + 46, 2));
+			#$chars[$num]{'sp'} = unpack("S1", substr($msg, $i + 46, 2));
 			$chars[$num]{'sp_max'} = unpack("S1", substr($msg, $i + 48, 2));
-			$chars[$num]{'sp_string'} = sprintf("%5d", $chars[$num]{'sp'})."/".sprintf("%-5d", $chars[$num]{'sp_max'}) if ($chars[$num]{'sp_max'} ne "");
-			$chars[$num]{'jobID'} = unpack("C1", substr($msg, $i + 52, 1));
+			$chars[$num]{'jobID'} = unpack("S1", substr($msg, $i + 52, 2));
 			$chars[$num]{'lv'} = unpack("C1", substr($msg, $i + 58, 1));
-			($chars[$num]{'name'}) = substr($msg, $i + 74, 24) =~ /([\s\S]*?)\000/;
+			($chars[$num]{'name'} = substr($msg, $i + 74, 24)) =~ s/\000.*$//s;
 			$chars[$num]{'str'} = unpack("C1", substr($msg, $i + 98, 1));
 			$chars[$num]{'agi'} = unpack("C1", substr($msg, $i + 99, 1));
 			$chars[$num]{'vit'} = unpack("C1", substr($msg, $i + 100, 1));
 			$chars[$num]{'int'} = unpack("C1", substr($msg, $i + 101, 1));
 			$chars[$num]{'dex'} = unpack("C1", substr($msg, $i + 102, 1));
 			$chars[$num]{'luk'} = unpack("C1", substr($msg, $i + 103, 1));
-			$chars[$num]{'sex'} = $sc_v{'input'}{'accountSex2'};
-			
-			$num = unpack("C1", substr($msg, $i + 104, 1));
-			($chars[$num]{'exp'},
-				$chars[$num]{'zenny'},
-				$chars[$num]{'exp_job'},
-				$chars[$num]{'lv_job'}) = unpack("x4 L3 C", substr($msg, $i, 106));
-			($chars[$num]{'hp_max'},
-				$chars[$num]{'sp_max'},
-				$chars[$num]{'jobID'},
-				$chars[$num]{'lv'}) = unpack("x42 x2 S x2 S x2 S x4 C", substr($msg, $i, 106));
-			($chars[$num]{'name'},
-				$chars[$num]{'str'},
-				$chars[$num]{'agi'},
-				$chars[$num]{'vit'},
-				$chars[$num]{'int'},
-				$chars[$num]{'dex'},
-				$chars[$num]{'luk'}) = unpack("x74 Z24 C6", substr($msg, $i, 106));
-			$chars[$num]{'sex'} = $sc_v{'input'}{'accountSex2'};
-			calPercent(\%{$chars[$num]}, "hp", unpack("S1", substr($msg, $i + 42, 2)));
-			calPercent(\%{$chars[$num]}, "sp", unpack("S1", substr($msg, $i + 46, 2)));
+#			($chars[$num]{'exp'},
+#				$chars[$num]{'zenny'},
+#				$chars[$num]{'exp_job'},
+#				$chars[$num]{'lv_job'}) = unpack("x4 L3 C", substr($msg, $i, 106));
+#			($chars[$num]{'hp_max'},
+#				$chars[$num]{'sp_max'},
+#				$chars[$num]{'jobID'},
+#				$chars[$num]{'lv'}) = unpack("x42 x2 S x2 S x2 S x4 C", substr($msg, $i, 106));
+#			($chars[$num]{'name'},
+#				$chars[$num]{'str'},
+#				$chars[$num]{'agi'},
+#				$chars[$num]{'vit'},
+#				$chars[$num]{'int'},
+#				$chars[$num]{'dex'},
+#				$chars[$num]{'luk'}) = unpack("x74 Z24 C6", substr($msg, $i, 106));
+#			$chars[$num]{'sex'} = $sc_v{'input'}{'accountSex2'};
+#			calPercent(\%{$chars[$num]}, "hp", unpack("S1", substr($msg, $i + 42, 2)));
+#			calPercent(\%{$chars[$num]}, "sp", unpack("S1", substr($msg, $i + 46, 2)));
 			
 			$chars[$num]{'hp_string'} = sprintf("%5d", $chars[$num]{'hp'})."/".sprintf("%-5d", $chars[$num]{'hp_max'}) if ($chars[$num]{'hp_max'} ne "");
 			$chars[$num]{'sp_string'} = sprintf("%5d", $chars[$num]{'sp'})."/".sprintf("%-5d", $chars[$num]{'sp_max'}) if ($chars[$num]{'sp_max'} ne "");
