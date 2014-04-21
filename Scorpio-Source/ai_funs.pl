1da8840a104cfba1c56a08d1c18a7cbbc78a5ae3
bluelovers <codelovers@users.sourceforge.net>
2014-04-22 07:18:13 +0800

    050723 (Step: 6)
    Step: 6

--- Scorpio-Source/ai_funs.pl
+++ Scorpio-Source/ai_funs.pl
@@ -158,154 +158,11 @@ sub ai_getSkillUseType {
 		, "AC_SHOWER"
 #		, "ASC_METEORASSAULT"
 
+#		, 'CR_GRANDCROSS'	#	¸t¤Q¦r¼f§P
+
 	);
 
 	return $val;
-}
-
-sub ai_mapRoute_getRoute {
-
-	my %args;
-
-	##VARS
-
-	$args{'g_normal'} = 1;
-
-	###
-
-	my ($returnArray, $r_start_field, $r_start_pos, $r_dest_field, $r_dest_pos, $time_giveup) = @_;
-	$args{'returnArray'} = $returnArray;
-	$args{'r_start_field'} = $r_start_field;
-	$args{'r_start_pos'} = $r_start_pos;
-	$args{'r_dest_field'} = $r_dest_field;
-	$args{'r_dest_pos'} = $r_dest_pos;
-	$args{'time_giveup'}{'timeout'} = $time_giveup;
-	$args{'time_giveup'}{'time'} = time;
-	unshift @ai_seq, "route_getMapRoute";
-	unshift @ai_seq_args, \%args;
-}
-
-sub ai_mapRoute_getSuccessors {
-	my ($r_args, $r_array, $r_cur) = @_;
-	my $ok;
-	foreach (keys %portals_lut) {
-		if ($portals_lut{$_}{'source'}{'map'} eq $$r_cur{'dest'}{'map'}
-
-			&& !($$r_cur{'source'}{'map'} eq $portals_lut{$_}{'dest'}{'map'}
-			&& $$r_cur{'source'}{'pos'}{'x'} == $portals_lut{$_}{'dest'}{'pos'}{'x'}
-			&& $$r_cur{'source'}{'pos'}{'y'} == $portals_lut{$_}{'dest'}{'pos'}{'y'})
-
-			&& !(%{$$r_cur{'parent'}} && $$r_cur{'parent'}{'source'}{'map'} eq $portals_lut{$_}{'dest'}{'map'}
-			&& $$r_cur{'parent'}{'source'}{'pos'}{'x'} == $portals_lut{$_}{'dest'}{'pos'}{'x'}
-			&& $$r_cur{'parent'}{'source'}{'pos'}{'y'} == $portals_lut{$_}{'dest'}{'pos'}{'y'})) {
-			undef $ok;
-			if (!%{$$r_cur{'parent'}}) {
-				if (!$$r_args{'solutions'}{$$r_args{'start'}{'dest'}{'field'}.\%{$$r_args{'start'}{'dest'}{'pos'}}.\%{$portals_lut{$_}{'source'}{'pos'}}}{'solutionTried'}) {
-					$$r_args{'solutions'}{$$r_args{'start'}{'dest'}{'field'}.\%{$$r_args{'start'}{'dest'}{'pos'}}.\%{$portals_lut{$_}{'source'}{'pos'}}}{'solutionTried'} = 1;
-					$timeout{'ai_route_calcRoute'}{'time'} -= $timeout{'ai_route_calcRoute'}{'timeout'};
-					$$r_args{'waitingForSolution'} = 1;
-					ai_route_getRoute(\@{$$r_args{'solutions'}{$$r_args{'start'}{'dest'}{'field'}.\%{$$r_args{'start'}{'dest'}{'pos'}}.\%{$portals_lut{$_}{'source'}{'pos'}}}{'solution'}},
-							$$r_args{'start'}{'dest'}{'field'}, \%{$$r_args{'start'}{'dest'}{'pos'}}, \%{$portals_lut{$_}{'source'}{'pos'}});
-					last;
-				}
-				$ok = 1 if (@{$$r_args{'solutions'}{$$r_args{'start'}{'dest'}{'field'}.\%{$$r_args{'start'}{'dest'}{'pos'}}.\%{$portals_lut{$_}{'source'}{'pos'}}}{'solution'}});
-			} elsif ($portals_los{$$r_cur{'dest'}{'ID'}}{$portals_lut{$_}{'source'}{'ID'}} ne "0"
-				&& $portals_los{$portals_lut{$_}{'source'}{'ID'}}{$$r_cur{'dest'}{'ID'}} ne "0") {
-				$ok = 1;
-			}
-			if ($$r_args{'dest'}{'source'}{'pos'}{'x'} ne "" && $portals_lut{$_}{'dest'}{'map'} eq $$r_args{'dest'}{'source'}{'map'}) {
-				if (!$$r_args{'solutions'}{$$r_args{'dest'}{'source'}{'field'}.\%{$portals_lut{$_}{'dest'}{'pos'}}.\%{$$r_args{'dest'}{'source'}{'pos'}}}{'solutionTried'}) {
-					$$r_args{'solutions'}{$$r_args{'dest'}{'source'}{'field'}.\%{$portals_lut{$_}{'dest'}{'pos'}}.\%{$$r_args{'dest'}{'source'}{'pos'}}}{'solutionTried'} = 1;
-					$timeout{'ai_route_calcRoute'}{'time'} -= $timeout{'ai_route_calcRoute'}{'timeout'};
-					$$r_args{'waitingForSolution'} = 1;
-					ai_route_getRoute(\@{$$r_args{'solutions'}{$$r_args{'dest'}{'source'}{'field'}.\%{$portals_lut{$_}{'dest'}{'pos'}}.\%{$$r_args{'dest'}{'source'}{'pos'}}}{'solution'}},
-							$$r_args{'dest'}{'source'}{'field'}, \%{$portals_lut{$_}{'dest'}{'pos'}}, \%{$$r_args{'dest'}{'source'}{'pos'}});
-					last;
-				}
-			}
-			push @{$r_array}, \%{$portals_lut{$_}} if $ok;
-		}
-	}
-}
-
-sub ai_mapRoute_searchStep {
-	my $r_args = shift;
-	my @successors;
-	my ($r_cur, $r_suc);
-	my $i;
-
-	###check if failed
-	if (!@{$$r_args{'openList'}}) {
-		#failed!
-		$$r_args{'done'} = 1;
-		return;
-	}
-
-	$r_cur = shift @{$$r_args{'openList'}};
-
-	###check if finished
-	if ($$r_args{'dest'}{'source'}{'map'} eq $$r_cur{'dest'}{'map'}
-		&& (@{$$r_args{'solutions'}{$$r_args{'dest'}{'source'}{'field'}.\%{$$r_cur{'dest'}{'pos'}}.\%{$$r_args{'dest'}{'source'}{'pos'}}}{'solution'}}
-		|| $$r_args{'dest'}{'source'}{'pos'}{'x'} eq "")) {
-		do {
-			unshift @{$$r_args{'solutionList'}}, {%{$r_cur}};
-			$r_cur = $$r_cur{'parent'} if (%{$$r_cur{'parent'}});
-		} while ($r_cur != \%{$$r_args{'start'}});
-		$$r_args{'done'} = 1;
-		return;
-	}
-
-	ai_mapRoute_getSuccessors($r_args, \@successors, $r_cur);
-	if ($$r_args{'waitingForSolution'}) {
-		undef $$r_args{'waitingForSolution'};
-		unshift @{$$r_args{'openList'}}, $r_cur;
-		return;
-	}
-
-	$newg = $$r_cur{'g'} + $$r_args{'g_normal'};
-	foreach $r_suc (@successors) {
-		undef $found;
-		undef $openFound;
-		undef $closedFound;
-		for($i = 0; $i < @{$$r_args{'openList'}}; $i++) {
-			if ($$r_suc{'dest'}{'map'} eq $$r_args{'openList'}[$i]{'dest'}{'map'}
-				&& $$r_suc{'dest'}{'pos'}{'x'} == $$r_args{'openList'}[$i]{'dest'}{'pos'}{'x'}
-				&& $$r_suc{'dest'}{'pos'}{'y'} == $$r_args{'openList'}[$i]{'dest'}{'pos'}{'y'}) {
-				if ($newg >= $$r_args{'openList'}[$i]{'g'}) {
-					$found = 1;
-					}
-				$openFound = $i;
-				last;
-			}
-		}
-		next if ($found);
-
-		undef $found;
-		for($i = 0; $i < @{$$r_args{'closedList'}}; $i++) {
-			if ($$r_suc{'dest'}{'map'} eq $$r_args{'closedList'}[$i]{'dest'}{'map'}
-				&& $$r_suc{'dest'}{'pos'}{'x'} == $$r_args{'closedList'}[$i]{'dest'}{'pos'}{'x'}
-				&& $$r_suc{'dest'}{'pos'}{'y'} == $$r_args{'closedList'}[$i]{'dest'}{'pos'}{'y'}) {
-				if ($newg >= $$r_args{'closedList'}[$i]{'g'}) {
-					$found = 1;
-				}
-				$closedFound = $i;
-				last;
-			}
-		}
-		next if ($found);
-		if ($openFound ne "") {
-			binRemoveAndShiftByIndex(\@{$$r_args{'openList'}}, $openFound);
-		}
-		if ($closedFound ne "") {
-			binRemoveAndShiftByIndex(\@{$$r_args{'closedList'}}, $closedFound);
-		}
-		$$r_suc{'g'} = $newg;
-		$$r_suc{'h'} = 0;
-		$$r_suc{'f'} = $$r_suc{'g'} + $$r_suc{'h'};
-		$$r_suc{'parent'} = $r_cur;
-		minHeapAdd(\@{$$r_args{'openList'}}, $r_suc, "f");
-	}
-	push @{$$r_args{'closedList'}}, $r_cur;
 }
 
 sub ai_items_take {
@@ -321,192 +178,6 @@ sub ai_items_take {
 	$args{'ai_items_take_start'}{'timeout'} = $timeout{'ai_items_take_start'}{'timeout'};
 	unshift @ai_seq, "items_take";
 	unshift @ai_seq_args, \%args;
-}
-
-sub ai_route {
-	my ($r_ret, $x, $y, $map, $maxRouteDistance, $maxRouteTime, $attackOnRoute, $avoidPortals, $distFromGoal, $checkInnerPortals) = @_;
-	my %args;
-	$x = int($x) if ($x ne "");
-	$y = int($y) if ($y ne "");
-	$args{'returnHash'} = $r_ret;
-	$args{'dest_x'} = $x;
-	$args{'dest_y'} = $y;
-	$args{'dest_map'} = $map;
-	$args{'maxRouteDistance'} = $maxRouteDistance;
-	$args{'maxRouteTime'} = $maxRouteTime;
-	$args{'attackOnRoute'} = $attackOnRoute;
-	$args{'avoidPortals'} = $avoidPortals;
-	$args{'distFromGoal'} = $distFromGoal;
-	$args{'checkInnerPortals'} = $checkInnerPortals;
-	undef %{$args{'returnHash'}};
-	unshift @ai_seq, "route";
-	unshift @ai_seq_args, \%args;
-	print "On route to: $maps_lut{$map.'.rsw'}($map): $x, $y\n" if ($config{'debug'});
-}
-
-sub ai_route_getDiagSuccessors {
-	my $r_args = shift;
-	my $r_pos = shift;
-	my $r_array = shift;
-	my $type = shift;
-	my %pos;
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}-1, $$r_pos{'y'}-1) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'}-1 && $$r_pos{'parent'}{'y'} == $$r_pos{'y'}-1)) {
-		$pos{'x'} = $$r_pos{'x'}-1;
-		$pos{'y'} = $$r_pos{'y'}-1;
-		push @{$r_array}, {%pos};
-	}
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}+1, $$r_pos{'y'}-1) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'}+1 && $$r_pos{'parent'}{'y'} == $$r_pos{'y'}-1)) {
-		$pos{'x'} = $$r_pos{'x'}+1;
-		$pos{'y'} = $$r_pos{'y'}-1;
-		push @{$r_array}, {%pos};
-	}
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}+1, $$r_pos{'y'}+1) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'}+1 && $$r_pos{'parent'}{'y'} == $$r_pos{'y'}+1)) {
-		$pos{'x'} = $$r_pos{'x'}+1;
-		$pos{'y'} = $$r_pos{'y'}+1;
-		push @{$r_array}, {%pos};
-	}
-
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}-1, $$r_pos{'y'}+1) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'}-1 && $$r_pos{'parent'}{'y'} == $$r_pos{'y'}+1)) {
-		$pos{'x'} = $$r_pos{'x'}-1;
-		$pos{'y'} = $$r_pos{'y'}+1;
-		push @{$r_array}, {%pos};
-	}
-}
-
-#Karasu Start
-sub ai_route_getMap {
-	my $r_args = shift;
-	my $x = shift;
-	my $y = shift;
-	return ai_route_getOffset(\%{$$r_args{'field'}}, $x, $y);
-}
-
-sub ai_route_getOffset {
-	my $r_args = shift;
-	my $x = shift;
-	my $y = shift;
-	$x = int($x); $y = int($y);
-	if ($x < 0 || $x >= $$r_args{'width'} || $y < 0 || $y >= $$r_args{'height'}) {
-		return 1;
-	}
-# Use substr instead of large array
-#	return $$r_args{'field'}[($y*$$r_args{'width'})+$x];
-	return unpack("C", substr($$r_args{'rawMap'}, ($y*$$r_args{'width'})+$x, 1));
-}
-#Karasu End
-
-sub ai_route_getRoute {
-	my %args;
-	my ($returnArray, $r_field, $r_start, $r_dest, $time_giveup) = @_;
-	$args{'returnArray'} = $returnArray;
-	$args{'field'} = $r_field;
-	%{$args{'start'}} = %{$r_start};
-	%{$args{'dest'}} = %{$r_dest};
-	$args{'time_giveup'}{'timeout'} = $time_giveup;
-	$args{'time_giveup'}{'time'} = time;
-	$args{'destroyFunction'} = \&ai_route_getRoute_destroy;
-	undef @{$args{'returnArray'}};
-	unshift @ai_seq, "route_getRoute";
-	unshift @ai_seq_args, \%args;
-}
-
-sub ai_route_getRoute_destroy {
-	my $r_args = shift;
-#ICE-WR Start
-	return if ($$r_args{'session'} eq "");
-#ICE-WR End
-	if (!$config{'buildType'}) {
-		$CalcPath_destroy->Call($$r_args{'session'});
-	} elsif ($config{'buildType'} == 1) {
-		&{$CalcPath_destroy}($$r_args{'session'});
-	}
-}
-sub ai_route_searchStep {
-	my $r_args = shift;
-	my $ret;
-
-	if (!$$r_args{'initialized'}) {
-		#####
-		my $SOLUTION_MAX = 5000;
-		$$r_args{'solution'} = "\0" x ($SOLUTION_MAX*4+4);
-		#####
-		if (!$config{'buildType'}) {
-			$$r_args{'session'} = $CalcPath_init->Call($$r_args{'solution'},
-				$$r_args{'field'}{'rawMap'}, $$r_args{'field'}{'width'}, $$r_args{'field'}{'height'},
-				pack("S*", $$r_args{'start'}{'x'}, $$r_args{'start'}{'y'}), pack("S*", $$r_args{'dest'}{'x'}, $$r_args{'dest'}{'y'}), $$r_args{'timeout'});
-		} elsif ($config{'buildType'} == 1) {
-			$$r_args{'session'} = &{$CalcPath_init}($$r_args{'solution'},
-				$$r_args{'field'}{'rawMap'}, $$r_args{'field'}{'width'}, $$r_args{'field'}{'height'},
-				pack("S*", $$r_args{'start'}{'x'}, $$r_args{'start'}{'y'}), pack("S*", $$r_args{'dest'}{'x'}, $$r_args{'dest'}{'y'}), $$r_args{'timeout'});
-
-		}
-	}
-	if ($$r_args{'session'} < 0) {
-		$$r_args{'done'} = 1;
-		return;
-	}
-	$$r_args{'initialized'} = 1;
-	if (!$config{'buildType'}) {
-		$ret = $CalcPath_pathStep->Call($$r_args{'session'});
-	} elsif ($config{'buildType'} == 1) {
-		$ret = &{$CalcPath_pathStep}($$r_args{'session'});
-	}
-	if (!$ret) {
-		my $size = unpack("L",substr($$r_args{'solution'},0,4));
-		my $j = 0;
-		my $i;
-		for ($i = ($size-1)*4+4; $i >= 4;$i-=4) {
-			$$r_args{'returnArray'}[$j]{'x'} = unpack("S",substr($$r_args{'solution'}, $i, 2));
-			$$r_args{'returnArray'}[$j]{'y'} = unpack("S",substr($$r_args{'solution'}, $i+2, 2));
-			$j++;
-		}
-		$$r_args{'done'} = 1;
-	}
-}
-
-sub ai_route_getSuccessors {
-	my $r_args = shift;
-	my $r_pos = shift;
-	my $r_array = shift;
-	my $type = shift;
-	my %pos;
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}-1, $$r_pos{'y'}) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'}-1 && $$r_pos{'parent'}{'y'} == $$r_pos{'y'})) {
-		$pos{'x'} = $$r_pos{'x'}-1;
-		$pos{'y'} = $$r_pos{'y'};
-		push @{$r_array}, {%pos};
-	}
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}, $$r_pos{'y'}-1) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'} && $$r_pos{'parent'}{'y'} == $$r_pos{'y'}-1)) {
-		$pos{'x'} = $$r_pos{'x'};
-		$pos{'y'} = $$r_pos{'y'}-1;
-		push @{$r_array}, {%pos};
-	}
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}+1, $$r_pos{'y'}) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'}+1 && $$r_pos{'parent'}{'y'} == $$r_pos{'y'})) {
-		$pos{'x'} = $$r_pos{'x'}+1;
-		$pos{'y'} = $$r_pos{'y'};
-		push @{$r_array}, {%pos};
-	}
-
-
-	if (ai_route_getMap($r_args, $$r_pos{'x'}, $$r_pos{'y'}+1) == $type
-		&& !($$r_pos{'parent'} && $$r_pos{'parent'}{'x'} == $$r_pos{'x'} && $$r_pos{'parent'}{'y'} == $$r_pos{'y'}+1)) {
-		$pos{'x'} = $$r_pos{'x'};
-		$pos{'y'} = $$r_pos{'y'}+1;
-		push @{$r_array}, {%pos};
-	}
 }
 
 #sellAuto for items_control - chobit andy 20030210
@@ -704,11 +375,15 @@ sub look {
 }
 
 sub move {
-	my $x = shift;
-	my $y = shift;
+	my $x	= shift;
+	my $y	= shift;
+	my $why	= shift;
+	my $ID	= shift;
 	my %args;
 	$args{'move_to'}{'x'} = $x;
 	$args{'move_to'}{'y'} = $y;
+	$args{'why'} = $why;
+	$args{'ID'} = $ID;
 	$args{'ai_move_giveup'}{'time'} = time;
 	$args{'ai_move_giveup'}{'timeout'} = $timeout{'ai_move_giveup'}{'timeout'};
 	unshift @ai_seq, "move";
@@ -749,6 +424,7 @@ sub take {
 	}
 
 	$args{'mode'} = $mode;
+	$args{'send'} = 0;
 #	if ($mode) {
 #		$args{'ai_take_giveup'}{'timeout'} = $timeout{'ai_take_giveup'}{'timeout'} + $timeout{'ai_take_giveup_important'}{'timeout'};
 #		$args{'mode'} = 1;
@@ -778,21 +454,39 @@ sub ai_equip {
 				(
 					$config{"equipAuto_$i"."_ignoreHpSp"}
 					|| (
-						percent_hp(\%{$chars[$config{'char'}]}) >= $config{"equipAuto_$i"."_hp_lower"}
-						&& percent_hp(\%{$chars[$config{'char'}]}) <= $config{"equipAuto_$i"."_hp_upper"}
-						&& percent_sp(\%{$chars[$config{'char'}]}) >= $config{"equipAuto_$i"."_sp_lower"}
-						&& percent_sp(\%{$chars[$config{'char'}]}) <= $config{"equipAuto_$i"."_sp_upper"}
+#						percent_hp(\%{$chars[$config{'char'}]}) >= $config{"equipAuto_$i"."_hp_lower"}
+#						&& percent_hp(\%{$chars[$config{'char'}]}) <= $config{"equipAuto_$i"."_hp_upper"}
+#						&& percent_sp(\%{$chars[$config{'char'}]}) >= $config{"equipAuto_$i"."_sp_lower"}
+#						&& percent_sp(\%{$chars[$config{'char'}]}) <= $config{"equipAuto_$i"."_sp_upper"}
+
+						mathInNum(percent_hp(\%{$chars[$config{'char'}]}), $config{"equipAuto_${i}_hp_upper"}, $config{"equipAuto_${i}_hp_lower"}, 1)
+						&& mathInNum(percent_sp(\%{$chars[$config{'char'}]}), $config{"equipAuto_${i}_sp_upper"}, $config{"equipAuto_${i}_sp_lower"}, 1)
 					)
 				)
 				&& (
 					!$config{"equipAuto_$i"."_monsters"}
-					|| $ai_index_attack ne ""
-					&& existsInList($config{"equipAuto_$i"."_monsters"}, $monsters{$ai_seq_args[$ai_index_attack]{'ID'}}{'name'})
+					|| (
+						$ai_index_attack ne ""
+						&& existsInList($config{"equipAuto_$i"."_monsters"}, $monsters{$ai_seq_args[$ai_index_attack]{'ID'}}{'name'})
+					)
 				)
 				&& (
+					!$config{"equipAuto_$i"."_monstersNot"}
+					|| (
+						$ai_index_attack ne ""
+						&& existsInList($config{"equipAuto_$i"."_monstersNot"}, $monsters{$ai_seq_args[$ai_index_attack]{'ID'}}{'name'})
+					)
+				)
+				&& ($config{"equipAuto_${i}_inCity"} || !$ai_v{'temp'}{'inCity'})
+				&& (!$config{"equipAuto_${i}_inLockOnly"} || ($config{"equipAuto_${i}_inLockOnly"} && $ai_v{'temp'}{'inLockMap'}))
+				&& (!$config{"equipAuto_${i}_unLockOnly"} || ($config{"equipAuto_${i}_unLockOnly"} && !$ai_v{'temp'}{'inLockMap'}))
+
+				&& (
 					!$config{"equipAuto_$i"."_skills"}
-					|| $ai_index_skill_use ne ""
-					&& existsInList($config{"equipAuto_$i"."_skills"}, $skillsID_lut{$ai_seq_args[$ai_index_skill_use]{'skill_use_id'}})
+					|| (
+						$ai_index_skill_use ne ""
+						&& existsInList($config{"equipAuto_$i"."_skills"}, $skillsID_lut{$ai_seq_args[$ai_index_skill_use]{'skill_use_id'}})
+					)
 				)
 				&& (
 					!$config{"equipAuto_$i"."_stopWhenCasting"}
@@ -825,8 +519,10 @@ sub ai_equip {
 
 			while (1) {
 				last if (!$config{$prefix."_$j"});
-				undef @array; splitUseArray(\@array, $config{$prefix."_$j"}, ",");
-				$ai_v{'temp'}{'equipAuto'}{'target'} = $array[0]; $ai_v{'temp'}{'equipAuto'}{'type_equip'} = $array[1];
+				undef @array;
+				splitUseArray(\@array, $config{$prefix."_$j"}, ",");
+				$ai_v{'temp'}{'equipAuto'}{'target'} = $array[0];
+				$ai_v{'temp'}{'equipAuto'}{'type_equip'} = $array[1];
 
 				if (
 					$ai_v{'temp'}{'equipAuto'}{'target'} eq "uneq"
@@ -854,6 +550,7 @@ sub ai_equip {
 							|| binFind(\@{$ai_v{'temp'}{'equipAuto'}{'sendEqed'}}, $k) ne ""
 							|| ($chars[$config{'char'}]{'autoSwitch'} ne "" && $chars[$config{'char'}]{'autoSwitch'} == $k)
 						);
+
 						if ($chars[$config{'char'}]{'inventory'}[$k]{'name'} eq $ai_v{'temp'}{'equipAuto'}{'target'}) {
 							# Check if the same eq already equipped
 							if ($ai_v{'temp'}{'equipAuto'}{'type_equip'} == 32 || $ai_v{'temp'}{'equipAuto'}{'type_equip'} == 128) {
@@ -878,6 +575,17 @@ sub ai_equip {
 								print "Auto-equip: $chars[$config{'char'}]{'inventory'}[$k]{'name'} ($k)\n" if ($config{'debug'});
 								$ai_v{'temp'}{'equipAuto'}{'packet_sent'}++;
 								$chars[$config{'char'}]{'autoSwitch'} = $k;
+
+								if ($config{"${prefix}_attackDistance"} && $config{"${prefix}_attackDistance"} != $config{'attackDistance'}) {
+									$ai_v{'ka'}{'attackDistance'} = $config{'attackDistance'};
+									$config{'attackDistance'} = $config{"${prefix}_attackDistance"};
+									print "Change Attack Distance to : $config{'attackDistance'}\n";
+								}
+								if ($config{"${prefix}_useWeapon"} ne "" && $config{"${prefix}_useWeapon"} != $config{'attackUseWeapon'}) {
+									$ai_v{'ka'}{'attackUseWeapon'} = $config{'attackUseWeapon'};
+									$config{'attackUseWeapon'} = $config{"${prefix}_useWeapon"};
+									print "Change Attack useWeapon to : $config{'attackUseWeapon'}\n";
+								}
 							}
 							last;
 						}
@@ -889,6 +597,17 @@ sub ai_equip {
 			last;
 		}
 		$i++;
+	}
+
+	return $ai_v{'temp'}{'equipAuto'}{'packet_sent'} if ($ai_v{'temp'}{'equipAuto'}{'packet_sent'});
+
+	if ($ai_v{'ka'}{'attackDistance'} && $config{'attackDistance'} != $ai_v{'ka'}{'attackDistance'}) {
+		$config{'attackDistance'} = $ai_v{'ka'}{'attackDistance'};
+		print "Change Attack Distance to Default : $config{'attackDistance'}\n";
+	}
+	if ($ai_v{'ka'}{'attackUseWeapon'} ne "" && $config{'attackUseWeapon'} != $ai_v{'ka'}{'attackUseWeapon'}) {
+		$config{'attackUseWeapon'} = $ai_v{'ka'}{'attackUseWeapon'};
+		print "Change Attack useWeapon to default : $config{'attackUseWeapon'}\n";
 	}
 }
 
